024fd4d70c9ddf29bd96d8a0ca5c245fc5739c8b
Patrick Steele-Idem <pnidem@gmail.com>
2016-03-29 16:38:22 -0600

    Fixes UMD wrapper - Check for exports first
    We've run into situations for an AMD library is
    being loaded a page that also includes a CommonJS
    module runtime. Giving more precedence to exports
    avoids problems.

--- index.js
+++ index.js
@@ -12,16 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// UMD HEADER START 
+// UMD HEADER START
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define([], factory);
-    } else if (typeof exports === 'object') {
+    if (typeof exports === 'object') {
         // Node. Does not work with strict CommonJS, but
         // only CommonJS-like enviroments that support module.exports,
         // like Node.
         module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
     } else {
         // Browser globals (root is window)
         root.returnExports = factory();
